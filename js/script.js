19203def82df9164da919d8c1dbc124eaa490bb3
Amit Vishwakarma <111479783+pglgod@users.noreply.github.com>
2023-02-05 00:22:56 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -37,19 +37,19 @@ function open_map() {
 
 
 // Swiper
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  grabCursor: true,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//  slidesPerView: 1,
+//  grabCursor: true,
+//  loop: true,
+//  pagination: {
+//    el: ".swiper-pagination",
+    // clickable: true,
+  // },
+  // navigation: {
+    // nextEl: ".swiper-button-next",
+    // prevEl: ".swiper-button-prev",
+  // },
+// });
 
 
 
